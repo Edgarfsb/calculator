9fc54c31b5133dc7884f34b99dec06de5ae53f2f
Edgar Baptista <91727653+Edgarfsb@users.noreply.github.com>
2026-07-01 01:29:34 +0100

    added updateDisplay function
    Added a function to display content on the display screen, updateDisplay()
    
    Added a bare bones function to listen to the button clicks and display a test message on the display

---
+++ script.js
@@ -0,0 +1,29 @@
+let displayValue = '0';
+const buttons = document.querySelectorAll('button');
+
+function updateDisplay() {
+    const display = document.getElementById('display');
+
+    display.innerText = displayValue;
+
+    if(displayValue.length > 9) {
+        display.innerText = displayValue.substring(0, 9);
+    }
+}
+
+// MADE TO TEST, NEEDS INPUT DIGIT FUNCTION!! NEEDS TO TAKE INTO ACCOUNT OTHER THINGS OTHER THAN DIGIT AND THE FUNCTIONS THAT TAKE CARE OF THEM
+// LIKE INPUT OPERATOR FUNCTION, EQUALS FUNCTION, CLEAR FUNCTION, BACKSPACE FUNCTION, DECIMAL FUNCTION
+// MAYBE MISSING SOME OTHER THINGS I AM FORGETTING
+function clickButton() {
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', function() {
+            if (buttons[i].classList.contains('digit')) {
+                displayValue = "123"; 
+                updateDisplay();
+            }
+        })
+    }
+}
+
+updateDisplay();
+clickButton();
