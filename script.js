d9e74321871eedaaa9403c2de356f262d5150979
Edgar Baptista <91727653+Edgarfsb@users.noreply.github.com>
2026-07-01 20:51:46 +0100

    added function to show result
    Added input equals function to show the result of the operations performed

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ let firstNumber = null;
 let secondNumber = null;
 let firstOperator = null;
 let secondOperator = null;
+let result = null;
 
 const buttons = document.querySelectorAll('button');
 
 function operate(x, y, operator) {
-    if(operator === '+') {
+    if (operator === '+') {
         return x + y;
-    } else if(operator === '-') {
+    } else if (operator === '-') {
         return x - y;
-    } else if(operator === '*') {
+    } else if (operator === '*') {
         return x * y;
-    } else if(operator === '/') {
-        if(y === 0) {
+    } else if (operator === '/') {
+        if (y === 0) {
             return 'nah';
         } else {
             return x / y;
@@ -41,7 +42,7 @@ function inputDigit(digit) {
 }
 
 function inputOperator(operator) {
-    if(firstOperator != null && secondOperator === null) {
+    if (firstOperator != null && secondOperator === null) {
         // Handles input of second operator
         secondOperator = operator;
         secondNumber = displayValue;
@@ -49,7 +50,7 @@ function inputOperator(operator) {
         displayValue = Math.round(result).toString();
         firstNumber = displayValue;
         result = null;
-    } else if(firstOperator != null && secondOperator != null) {
+    } else if (firstOperator != null && secondOperator != null) {
         // For new secondOperator
         secondNumber = displayValue;
         result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
@@ -64,19 +65,58 @@ function inputOperator(operator) {
     }
 }
 
+function inputEquals() {
+    // Hitting equals doesn't display undefined before operate()
+    if (firstOperator === null) {
+        displayValue = displayValue;
+    } else if (secondOperator != null) {
+        // Handles final result
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), secondOperator);
+
+        if (result === 'nah') {
+            displayValue = 'nah bro';
+        } else {
+            displayValue = Math.round(result).toString();
+            firstNumber = displayValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    } else {
+        // Handles first operation
+        secondNumber = displayValue;
+        result = operate(Number(firstNumber), Number(secondNumber), firstOperator);
+
+        if (result === 'nah') {
+            displayValue = 'nah bro';
+        } else {
+            displayValue = Math.round(result).toString();
+            firstNumber = displayValue;
+            secondNumber = null;
+            firstOperator = null;
+            secondOperator = null;
+            result = null;
+        }
+    }
+}
+
 function updateDisplay() {
     const display = document.getElementById('display');
 
     display.innerText = displayValue;
 
-    if(displayValue.length > 9) {
+    if (displayValue.length > 9) {
         display.innerText = displayValue.substring(0, 9);
     }
 }
 
-// MADE TO TEST, NEEDS INPUT DIGIT FUNCTION!! NEEDS TO TAKE INTO ACCOUNT OTHER THINGS OTHER THAN DIGIT AND THE FUNCTIONS THAT TAKE CARE OF THEM
-// LIKE INPUT OPERATOR FUNCTION, EQUALS FUNCTION, CLEAR FUNCTION, BACKSPACE FUNCTION, DECIMAL FUNCTION
-// MAYBE MISSING SOME OTHER THINGS I AM FORGETTING
+// MISSING BUTTONS
+//BACKSPACE
+//DECIMAL
+//CLEAR
+//MAYBE ADD A FUNCTION THAT ROUNDS USING SCIENTIFIC NOTATION FOR NUMBERS TOO BIG OR TOO SMALL
 function clickButton() {
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function() {
@@ -85,6 +125,10 @@ function clickButton() {
                 updateDisplay();
             } else if (buttons[i].classList.contains('operator')) {
                 inputOperator(buttons[i].value);
+                updateDisplay();
+            } else if (buttons[i].classList.contains('equals')) {
+                inputEquals();
+                updateDisplay();
             }
         })
     }
